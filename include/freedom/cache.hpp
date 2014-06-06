e9991261813c27b84a40688316e691f21d108e36
pandaant <apandaant@gmail.com>
2014-06-06 09:09:12 +0000

    cache class begun

---
+++ include/freedom/cache.hpp
@@ -0,0 +1,14 @@
+#ifndef CACHE_H
+#define CACHE_H
+
+namespace freedom {
+
+template <typename Obj> class Cache {
+    public:
+        Cache(const unsigned &max_entries = 0);
+
+        
+};
+}
+
+#endif
